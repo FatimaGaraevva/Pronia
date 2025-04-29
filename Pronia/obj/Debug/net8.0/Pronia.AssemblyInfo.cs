71b233697170ad04b4ae259138a539e74715d413
FatimaGaraevva <fatimagr-bp220@code.edu.az>
2025-04-29 23:55:18 +0400

    null exception handling

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9831f7a930a0c1f2e191875d56a38e64f654a2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e036380e206e0da26407979e4b8e6b92799fdbf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
