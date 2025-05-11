4ee9f3c03c7e93e5721d6052a1d6f76d3ce608e0
FatimaGaraevva <fatimagr-bp220@code.edu.az>
2025-05-11 22:29:31 +0400

    product update,create,delete

--- Pronia/Areas/Admin/Controllers/ProductController.cs
+++ Pronia/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pronia.DAL;
+using Pronia.Models;
+using Pronia.Utilities.Enams;
+using Pronia.Utilities.Extensions;
 using Pronia.ViewModels;
 
 namespace Pronia.Areas.Admin.Controllers
@@ -9,10 +12,12 @@ namespace Pronia.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public ProductController(AppDbContext context)
+        public ProductController(AppDbContext context,IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
         public async Task< IActionResult> Index()
         {
@@ -28,5 +33,177 @@ namespace Pronia.Areas.Admin.Controllers
             }).ToListAsync();
             return View(productsVMs);
         }
+        public async Task<IActionResult> Create()
+        {
+            CreateProductVM productVM = new CreateProductVM
+            {
+                Categories = await _context.Categories.ToListAsync()
+            };
+            return View(productVM);
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductVM productVM)
+        {
+            productVM.Categories = await _context.Categories.ToListAsync();
+            if (!ModelState.IsValid)
+            {
+                return View(productVM);
+            }
+            bool result = productVM.Categories.Any(c => c.Id == productVM.CategoryId);
+            if (!result)
+            {
+                ModelState.AddModelError(nameof(CreateProductVM.CategoryId), "category dose not exsist");
+                return View(productVM);
+            }
+            if (!productVM.MainPhoto.ValidateType("image/"))
+            {
+                ModelState.AddModelError(nameof(CreateProductVM.MainPhoto), "File type is inccorect");
+                return View(productVM);
+            }
+            if (!productVM.MainPhoto.ValidateSize(FileSize.KB,500))
+            {
+                ModelState.AddModelError(nameof(CreateProductVM.MainPhoto), "File size must be less than 500 KB");
+                return View(productVM);
+            }
+            bool nameResult = await _context.Products.AnyAsync(p => p.Name == productVM.Name);
+            if (nameResult)
+            {
+                ModelState.AddModelError(nameof(CreateProductVM.Name), $"Product with{productVM.Name} already exists");
+                return View(productVM);
+            }
+            ProductImage main = new ProductImage
+            {
+                Image = await productVM.MainPhoto.CreateFileAsync(_env.WebRootPath, "assets", "images", "website-images"),
+                IsPrimary = true,
+                CreateAt=DateTime.Now
+            };
+            Product product = new Product
+            {
+                Name = productVM.Name,
+                Price = productVM.Price.Value,
+                SKU = productVM.SKU,
+                Description=productVM.Description,
+                CategoryId=productVM.CategoryId.Value,
+                ProductImages= new  List<ProductImage> { main}
+
+            };
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Update(int?id)
+        {
+            if (id is null || id<=0)
+            {
+                return BadRequest();
+            }
+            Product? product = await _context.Products
+            .Include(p => p.ProductImages)
+            .FirstOrDefaultAsync(p => p.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            UpdateProductVM productVM = new UpdateProductVM
+            {
+                Name = product.Name,
+                SKU = product.SKU,
+                Description = product.Description,
+                Price = product.Price,
+                CategoryId = product.CategoryId,
+                PrimaryImage = product.ProductImages.FirstOrDefault(p => p.IsPrimary == true).Image,
+                Categories = await _context.Categories.ToListAsync()
+
+            };
+            return View(productVM);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id,UpdateProductVM productVM)
+        {
+            productVM.Categories = await _context.Categories.ToListAsync();
+            if (!ModelState.IsValid)
+            {
+                return View(productVM);
+            }
+            if (productVM.MainPhoto is not null)
+            {
+                if (!productVM.MainPhoto.ValidateType("image/"))
+                {
+                    ModelState.AddModelError(nameof(UpdateProductVM.MainPhoto), "File type is inccorect");
+                    return View(productVM);
+                }
+                if (!productVM.MainPhoto.ValidateSize(FileSize.KB, 500))
+                {
+                    ModelState.AddModelError(nameof(UpdateProductVM.MainPhoto), "File size must be less than 500 KB");
+                    return View(productVM);
+                }
+            }
+            bool result = productVM.Categories.Any(c => c.Id == productVM.CategoryId);
+            if (!result)
+            {
+                ModelState.AddModelError(nameof(UpdateProductVM.CategoryId), "category does not exists");
+                return View(productVM);
+            }
+            bool nameResult = await _context.Products.AnyAsync(p => p.Name == productVM.Name && p.Id != id);
+            if (nameResult)
+            {
+                ModelState.AddModelError(nameof(UpdateProductVM.Name), "product alerady exists");
+                return View(productVM);
+            }
+            Product? existed = await _context.Products
+            .Include(p => p.ProductImages)
+            .FirstOrDefaultAsync(p => p.Id == id);
+            if (productVM.MainPhoto is not null)
+            {
+                ProductImage main = new ProductImage
+                {
+                    Image = await productVM.MainPhoto.CreateFileAsync(_env.WebRootPath, "assets", "images", "website-images"),
+                    IsPrimary = true,
+                    CreateAt = DateTime.Now
+                };
+                ProductImage existedMain = existed.ProductImages.FirstOrDefault(pi => pi.IsPrimary == true);
+                existedMain.Image.DeleteFile(_env.WebRootPath, "assets", "images", "website-images");
+                existed.ProductImages.Remove(existedMain);
+                existed.ProductImages.Add(main);
+            }
+            existed.Name = productVM.Name;
+            existed.Price = productVM.Price.Value;
+            existed.Description = productVM.Description;
+            existed.CategoryId = productVM.CategoryId.Value;
+            existed.SKU = productVM.SKU;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null || id <= 0)
+            {
+                return BadRequest();
+            }
+            Product? product = await _context.Products
+            .Include(p => p.ProductImages)
+            .FirstOrDefaultAsync(p => p.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            foreach ( ProductImage protImage in product.ProductImages)
+            {
+                protImage.Image.DeleteFile(_env.WebRootPath, "assets", "images", "website-images");
+
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
     }
 }
--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c528a86e91cc5c8b5a0bc377fa4223c7d3a966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a485f11afec3441c1cc2295f0fcb643b2a6bc47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
